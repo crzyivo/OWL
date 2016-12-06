a8bba6b094fdce9e2d765a2abee3d5908e07218e
Hectorowl <hector.ninian@gmail.com>
2016-12-06 18:17:36 +0100

    Add files via upload

---
+++ OWL/src/Data/LibroVO.java
@@ -0,0 +1,69 @@
+
+package Data;
+
+public class LibroVO{
+	
+	private int id;
+	private String titulo;
+	private String autor; 
+	private String descripcion;
+	private Integer ventas;
+
+	
+	public LibroVO() {
+		this.id = -1;
+		this.titulo = "Libro Vacío";
+		this.autor= "OWL Database" ;
+		this.descripcion= "Creado por el constructor vacío";
+		this.ventas=0;
+		
+	}
+	
+	public LibroVO( int id, String titulo, String autor, String descripcion, Integer ventas){
+		this.id = id;
+		this.titulo = titulo;
+		this.autor= autor ;
+		this.descripcion= descripcion;
+		this.ventas= ventas;	
+	}
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return this.titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return this.autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion =descripcion;
+	}
+
+	public Integer getVentas() {
+		return this.ventas;
+	}
+
+	public void setVentas(Integer ventas) {
+		this.ventas = ventas;
+	}
+	
+};
