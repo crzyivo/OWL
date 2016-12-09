bf0e81b0d8577adf5a8a6dc0c2cdcca3d38a97a5
David Abián <da@davidabian.com>
2016-12-09 07:10:47 +0100

    Add files via upload

--- OWL/src/facades/EjemplarFacade.java
+++ OWL/src/facades/EjemplarFacade.java
@@ -11,83 +11,85 @@ import java.util.List;
 
 import java.sql.Connection;
 
-public class EjemplarFacade{
-	
-	public void insertarNuevoEjemplar(EjemplarVO ejemplar,List<String> errores) throws SQLException{
-		JDBCTemplate mysql=null;
+public class EjemplarFacade {
+
+	public void insertarNuevoEjemplar(EjemplarVO ejemplar, List<String> errores) throws SQLException {
+		JDBCTemplate mysql = null;
 		EjemplarVO test = new EjemplarVO();
-		try{
-			mysql=MysqlConnection.getConnection();
+		try {
+			mysql = MysqlConnection.getConnection();
 			mysql.connect();
-			EjemplarDAO OwlDAO=new EjemplarDAO();
-			//ErrorsStrings.compruebaDatos(usuario,errores);
-			//TODO: Control de errores mas fino si da tiempo
-			verEjemplar(ejemplar.getId(),test);
-			if(test.getEditorial()!=null){
+			EjemplarDAO OwlDAO = new EjemplarDAO();
+			// ErrorsStrings.compruebaDatos(usuario,errores);
+			// TODO: Control de errores mas fino si da tiempo
+			verEjemplar(ejemplar.getId(), test);
+			if (test.getEditorial() != null) {
 				System.out.println("Repetido");
-			}
-			else{
-			//PasswordAuthentication passauth= new PasswordAuthentication();
-			//String hashpass=passauth.hash(usuario.getPassword());
-			//usuario.setPassword(hashpass);
-			OwlDAO.insertarEjemplar(ejemplar,mysql);
+			} else {
+				// PasswordAuthentication passauth= new
+				// PasswordAuthentication();
+				// String hashpass=passauth.hash(usuario.getPassword());
+				// usuario.setPassword(hashpass);
+				OwlDAO.insertarEjemplar(ejemplar, mysql);
 			}
 			mysql.disconnect();
-			
-			}catch (Exception e) {
-					errores.add(e.getMessage());
-				       e.printStackTrace(System.err);
-				       
-			}finally{
-				mysql.disconnect();
-			}
-		
+
+		} catch (Exception e) {
+			errores.add(e.getMessage());
+			e.printStackTrace(System.err);
+
+		} finally {
+			mysql.disconnect();
+		}
+
 	}
-	
-	public void modificarEjemplar(EjemplarVO ejemplar){
-		JDBCTemplate mysql=MysqlConnection.getConnection();
-		try{
+
+	public void modificarEjemplar(EjemplarVO ejemplar) {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
 			mysql.connect();
-			EjemplarDAO OwlDAO=new EjemplarDAO();
-			OwlDAO.actualizarEjemplar(ejemplar,mysql);
+			EjemplarDAO OwlDAO = new EjemplarDAO();
+			OwlDAO.actualizarEjemplar(ejemplar, mysql);
 			mysql.disconnect();
-			
-			}catch (Exception e) {
-				       e.printStackTrace(System.err);
-			}finally{
-				mysql.disconnect();
-			}
-		
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		} finally {
+			mysql.disconnect();
+		}
+
 	}
-	//Obtiene datos de un ejemplar a partir de su id.
-	public void verEjemplar(int id,EjemplarVO ejemplardata){
-		JDBCTemplate mysql=MysqlConnection.getConnection();
-		try{
+
+	// Obtiene datos de un ejemplar a partir de su id.
+	public void verEjemplar(int id, EjemplarVO ejemplardata) {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
 			mysql.connect();
-			EjemplarDAO OwlDAO=new EjemplarDAO();
-			ejemplardata=OwlDAO.obtenerEjemplar(id,mysql);
+			EjemplarDAO OwlDAO = new EjemplarDAO();
+			ejemplardata = OwlDAO.obtenerEjemplar(id, mysql);
 			mysql.disconnect();
-			
-			}catch (Exception e) {
-				       e.printStackTrace(System.err);
-			}finally{
-				mysql.disconnect();
-			}
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		} finally {
+			mysql.disconnect();
+		}
 	}
-	public void borrarEjemplar(int id){
-		JDBCTemplate mysql=MysqlConnection.getConnection();
-		try{
+
+	public void borrarEjemplar(int id) {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
 			mysql.connect();
-			EjemplarDAO OwlDAO=new EjemplarDAO();
-			OwlDAO.borrarEjemplar(id,mysql);
+			EjemplarDAO OwlDAO = new EjemplarDAO();
+			OwlDAO.borrarEjemplar(id, mysql);
 			mysql.disconnect();
-			
-			}catch (Exception e) {
-				       e.printStackTrace(System.err);
-			}finally{
-				mysql.disconnect();
-			}
-		
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		} finally {
+			mysql.disconnect();
+		}
+
 	}
-	
+
 }
--- OWL/src/facades/OWLFacade.java
+++ OWL/src/facades/OWLFacade.java
@@ -19,246 +19,312 @@ import facades.ErrorsStrings;
 
 import java.sql.Connection;
 
-public class OWLFacade{
-	
-	public void insertarNuevoUsuario(OwlUserVO usuario,List<String> errores) throws SQLException{
-		JDBCTemplate mysql=null;
+public class OWLFacade {
+
+	public void insertarNuevoUsuario(OwlUserVO usuario, List<String> errores) throws SQLException {
+		JDBCTemplate mysql = null;
 		OwlUserVO test = new OwlUserVO();
-		try{
-			mysql=MysqlConnection.getConnection();
+		try {
+			mysql = MysqlConnection.getConnection();
 			mysql.connect();
-			OwlUserDAO OwlDAO=new OwlUserDAO();
-			ErrorsStrings.compruebaDatos(usuario,errores);
-			if(errores.isEmpty()){
+			OwlUserDAO OwlDAO = new OwlUserDAO();
+			ErrorsStrings.compruebaDatos(usuario, errores);
+			if (errores.isEmpty()) {
 				test = verUsuario(usuario.getEmail());
-				if(test.getEmail()!=null){
+				if (test.getEmail() != null) {
 					errores.add("Este correo ya esta en uso");
-				}
-				else{
-				PasswordAuthentication passauth= new PasswordAuthentication();
-				String hashpass=passauth.hash(usuario.getPassword());
-				usuario.setPassword(hashpass);
-				OwlDAO.insertarUsuario(usuario,mysql);
-				}
-			}
-			mysql.disconnect();
-			
-			}catch (Exception e) {
-					errores.add(e.getMessage());
-				       e.printStackTrace(System.err);
-				       
-			}finally{
-				mysql.disconnect();
-			}
-		
-	}
-	
-	public void modificarUsuario(OwlUserVO usuario,boolean pass,List<String> errores){
-		JDBCTemplate mysql=MysqlConnection.getConnection();
-		try{
-			mysql.connect();
-			OwlUserDAO OwlDAO=new OwlUserDAO();
-			if(pass){
-				ErrorsStrings.compruebaDatos(usuario,errores);
-				if(errores.isEmpty()){
-				PasswordAuthentication passauth= new PasswordAuthentication();
-				String hashpass=passauth.hash(usuario.getPassword());
-				usuario.setPassword(hashpass);
-				OwlDAO.actualizarUsuario(usuario,mysql);
-				}
-			}
-			else{
-				ErrorsStrings.compruebaDatos(usuario,errores);
-				if(errores.isEmpty()){
-					OwlDAO.actualizarUsuario(usuario,mysql);
+				} else {
+					PasswordAuthentication passauth = new PasswordAuthentication();
+					String hashpass = passauth.hash(usuario.getPassword());
+					usuario.setPassword(hashpass);
+					OwlDAO.insertarUsuario(usuario, mysql);
 				}
 			}
 			mysql.disconnect();
-			
-			}catch (Exception e) {
-				       e.printStackTrace(System.err);
-			}finally{
-				mysql.disconnect();
-			}
-		
+
+		} catch (Exception e) {
+			errores.add(e.getMessage());
+			e.printStackTrace(System.err);
+
+		} finally {
+			mysql.disconnect();
+		}
+
 	}
-	//Obtiene datos de un usuario a partir de su mail.
-	public OwlUserVO verUsuario(String email){
-		JDBCTemplate mysql=MysqlConnection.getConnection();
-		try{
+
+	public void modificarUsuario(OwlUserVO usuario, boolean pass, List<String> errores) {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
 			mysql.connect();
-			OwlUserDAO OwlDAO=new OwlUserDAO();
-			OwlUserVO userdata=OwlDAO.obtenerUsuario(email,mysql);
+			OwlUserDAO OwlDAO = new OwlUserDAO();
+			if (pass) {
+				ErrorsStrings.compruebaDatos(usuario, errores);
+				if (errores.isEmpty()) {
+					PasswordAuthentication passauth = new PasswordAuthentication();
+					String hashpass = passauth.hash(usuario.getPassword());
+					usuario.setPassword(hashpass);
+					OwlDAO.actualizarUsuario(usuario, mysql);
+				}
+			} else {
+				ErrorsStrings.compruebaDatos(usuario, errores);
+				if (errores.isEmpty()) {
+					OwlDAO.actualizarUsuario(usuario, mysql);
+				}
+			}
+			mysql.disconnect();
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		} finally {
+			mysql.disconnect();
+		}
+
+	}
+
+	// Obtiene datos de un usuario a partir de su mail.
+	public OwlUserVO verUsuario(String email) {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
+			mysql.connect();
+			OwlUserDAO OwlDAO = new OwlUserDAO();
+			OwlUserVO userdata = OwlDAO.obtenerUsuario(email, mysql);
 			mysql.disconnect();
 			return userdata;
-			
-			}catch (Exception e) {
-				       e.printStackTrace(System.err);
-				       return null;
-			}finally{
-				mysql.disconnect();
-			}
-	}
-	public void borrarCuenta(String email){
-		JDBCTemplate mysql=MysqlConnection.getConnection();
-		try{
-			mysql.connect();
-			OwlUserDAO OwlDAO=new OwlUserDAO();
-			OwlDAO.borrarUsuario(email,mysql);
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			return null;
+		} finally {
 			mysql.disconnect();
-			
-			}catch (Exception e) {
-				       e.printStackTrace(System.err);
-			}finally{
-				mysql.disconnect();
-			}
-		
+		}
 	}
-	
-	public boolean comprobarLogin(String usuario,String pass){
-		JDBCTemplate mysql=MysqlConnection.getConnection();
-		try{
+
+	public void borrarCuenta(String email) {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
 			mysql.connect();
-			OwlUserDAO OwlDAO=new OwlUserDAO();
-			String current=OwlDAO.obtenerUsuario(usuario,mysql).getPassword();
+			OwlUserDAO OwlDAO = new OwlUserDAO();
+			OwlDAO.borrarUsuario(email, mysql);
 			mysql.disconnect();
-			PasswordAuthentication passauth= new PasswordAuthentication();
-			return passauth.authenticate(pass,current);
-			
-			
-			}catch (Exception e) {
-				       e.printStackTrace(System.err);
-				       return false;
-			}finally{
-				mysql.disconnect();
-			}
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		} finally {
+			mysql.disconnect();
+		}
+
 	}
-	
-	public List<LibroVO> librosPorCategoria(String cat){
-		JDBCTemplate mysql=MysqlConnection.getConnection();
-		try{
+
+	public boolean comprobarLogin(String usuario, String pass) {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
 			mysql.connect();
-			CategoriaDAO OwlDAO=new CategoriaDAO();
-			List<LibroVO>resultado = new ArrayList();
-			resultado=OwlDAO.obtenerLibrosInCategoria(cat,mysql,"lecturas");
-			if(resultado.isEmpty()){
+			OwlUserDAO OwlDAO = new OwlUserDAO();
+			String current = OwlDAO.obtenerUsuario(usuario, mysql).getPassword();
+			mysql.disconnect();
+			PasswordAuthentication passauth = new PasswordAuthentication();
+			return passauth.authenticate(pass, current);
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			return false;
+		} finally {
+			mysql.disconnect();
+		}
+	}
+
+	public List<LibroVO> librosPorCategoria(String cat) {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
+			mysql.connect();
+			CategoriaDAO OwlDAO = new CategoriaDAO();
+			List<LibroVO> resultado = new ArrayList();
+			resultado = OwlDAO.obtenerLibrosInCategoria(cat, mysql, "lecturas");
+			if (resultado.isEmpty()) {
 				mysql.disconnect();
 				return null;
-				
-			}else{
+
+			} else {
 				mysql.disconnect();
 				return resultado;
 			}
-			
-			}catch (Exception e) {
-				       e.printStackTrace(System.err);
-				       return null;
-			}finally{
-				mysql.disconnect();
-			}
-}
-	public void librosPorCategorian(){
-		JDBCTemplate mysql=MysqlConnection.getConnection();
-		try{
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			return null;
+		} finally {
+			mysql.disconnect();
+		}
+	}
+
+	public LibroVO VerLibro(int id) {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
 			mysql.connect();
-			CategoriaDAO OwlDAO=new CategoriaDAO();
-			List<LibroVO>resultado = new ArrayList();
+			LibroDAO OwlDAO = new LibroDAO();
+			LibroVO resultado = new LibroVO();
+			resultado = OwlDAO.obtenerlibro(id, mysql);
+			if (resultado.getId() == -1) {
+				mysql.disconnect();
+				return null;
+
+			} else {
+				mysql.disconnect();
+				return resultado;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			return null;
+		} finally {
+			mysql.disconnect();
+		}
+	}
+
+	public void librosPorCategorian() {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
+			mysql.connect();
+			CategoriaDAO OwlDAO = new CategoriaDAO();
+			List<LibroVO> resultado = new ArrayList();
 			OwlDAO.actualizarCategorian(mysql);
-				
-			}catch (Exception e) {
-				       e.printStackTrace(System.err);
-				       
-			}finally{
-				mysql.disconnect();
-			}
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+
+		} finally {
+			mysql.disconnect();
+		}
 	}
-	public List<CategoriaVO> mostrarCategorias(){
-		JDBCTemplate mysql=MysqlConnection.getConnection();
-		try{
+
+	public List<CategoriaVO> mostrarCategorias() {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
 			mysql.connect();
-			CategoriaDAO OwlDAO=new CategoriaDAO();
-			List<CategoriaVO>resultado = new ArrayList();
-			resultado=OwlDAO.obtenerCategorias(mysql);
+			CategoriaDAO OwlDAO = new CategoriaDAO();
+			List<CategoriaVO> resultado = new ArrayList();
+			resultado = OwlDAO.obtenerCategorias(mysql);
 			return resultado;
-				
-			}catch (Exception e) {
-				       e.printStackTrace(System.err);
-				       return null;
-				       
-			}finally{
-				mysql.disconnect();
-			}
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			return null;
+
+		} finally {
+			mysql.disconnect();
+		}
 	}
-	
-	public List<LibroVO> librosMasLeidos(){
-		JDBCTemplate mysql=MysqlConnection.getConnection();
-		try{
+
+	public List<LibroVO> librosMasLeidos() {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
 			mysql.connect();
-			LibroDAO OwlDAO=new LibroDAO();
-			List<LibroVO>resultado = new ArrayList();
-			resultado=OwlDAO.obtenerLibrosByVenta(mysql);
-			if(resultado.isEmpty()){
+			LibroDAO OwlDAO = new LibroDAO();
+			List<LibroVO> resultado = new ArrayList();
+			resultado = OwlDAO.obtenerLibrosByVenta(mysql);
+			if (resultado.isEmpty()) {
 				mysql.disconnect();
 				return null;
-				
-			}else{
+
+			} else {
 				mysql.disconnect();
 				return resultado;
 			}
-			
-			}catch (Exception e) {
-				       e.printStackTrace(System.err);
-				       return null;
-			}finally{
-				mysql.disconnect();
-			}
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			return null;
+		} finally {
+			mysql.disconnect();
+		}
 	}
-	public List<EjemplarVO> EjemplaresLibro(int id){
-		JDBCTemplate mysql=MysqlConnection.getConnection();
-		try{
+
+	public List<EjemplarVO> EjemplaresLibro(int id) {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
 			mysql.connect();
-			EjemplarDAO OwlDAO=new EjemplarDAO();
-			List<EjemplarVO>resultado = new ArrayList();
-			resultado=OwlDAO.obtenerEjemplaresBy("libro","",id,mysql);
-			if(resultado.isEmpty()){
+			EjemplarDAO OwlDAO = new EjemplarDAO();
+			List<EjemplarVO> resultado = new ArrayList();
+			resultado = OwlDAO.obtenerEjemplaresBy("libro", "", id, mysql);
+			if (resultado.isEmpty()) {
 				mysql.disconnect();
 				return new ArrayList();
-				
-			}else{
+
+			} else {
 				mysql.disconnect();
 				return resultado;
 			}
-			
-			}catch (Exception e) {
-				       e.printStackTrace(System.err);
-				       return null;
-			}finally{
-				mysql.disconnect();
-			}
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			return null;
+		} finally {
+			mysql.disconnect();
+		}
 	}
-	
-	public void insertarNuevoEjemplar(EjemplarVO ejemplar,List<String> errores) throws SQLException{
-		JDBCTemplate mysql=null;
-		try{
-			mysql=MysqlConnection.getConnection();
+
+	public void insertarNuevoEjemplar(EjemplarVO ejemplar, List<String> errores) throws SQLException {
+		JDBCTemplate mysql = null;
+		try {
+			mysql = MysqlConnection.getConnection();
 			mysql.connect();
-			EjemplarDAO OwlDAO=new EjemplarDAO();
-			//ErrorsStrings.compruebaDatos(usuario,errores);
-			if(errores.isEmpty()){
+			EjemplarDAO OwlDAO = new EjemplarDAO();
+			ErrorsStringsEjemplares.compruebaDatos(ejemplar, errores);
+			if (errores.isEmpty()) {
 				OwlDAO.insertarEjemplar(ejemplar, mysql);
 
 			}
 			mysql.disconnect();
-			
-			}catch (Exception e) {
-					errores.add(e.getMessage());
-				       e.printStackTrace(System.err);
-				       
-			}finally{
-				mysql.disconnect();
-			}
-		
+
+		} catch (Exception e) {
+			errores.add(e.getMessage());
+			e.printStackTrace(System.err);
+
+		} finally {
+			mysql.disconnect();
+		}
+
 	}
-	
-	
+
+	public List<List<EjemplarVO>> movimientosUsuario(String user) {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
+			mysql.connect();
+			EjemplarDAO OwlDAO = new EjemplarDAO();
+			List<EjemplarVO> ventas = OwlDAO.obtenerEjemplaresBy("vendedor", user, 0, mysql);
+			List<EjemplarVO> compras = OwlDAO.obtenerEjemplaresBy("comprador", user, 0, mysql);
+			List<List<EjemplarVO>> resultado = new ArrayList();
+			resultado.add(ventas);
+			resultado.add(compras);
+			return resultado;
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			return null;
+
+		} finally {
+			mysql.disconnect();
+		}
+	}
+
+	public EjemplarVO realizarCompra(String user, int id, String fcompra) {
+		JDBCTemplate mysql = MysqlConnection.getConnection();
+		try {
+			mysql.connect();
+			EjemplarDAO OwlDAO = new EjemplarDAO();
+			EjemplarVO ejemplar = OwlDAO.obtenerEjemplar(id, mysql);
+			ejemplar.setComprador(user);
+			ejemplar.setfcompra(fcompra);
+			OwlDAO.actualizarEjemplar(ejemplar, mysql);
+			return ejemplar;
+
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			return null;
+
+		} finally {
+			mysql.disconnect();
+		}
+	}
+
 }
